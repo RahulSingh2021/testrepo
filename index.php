b7e812ec6707e063ea67cf33cd34c2f179f054a8
Rahul Home <119575905+Rahulsinghrock1@users.noreply.github.com>
2026-05-17 14:46:28 +0530

    fdfdsf

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
       <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav mx-auto">
         <li class="nav-item dropdown active dropdown-slide">
-          <a class="nav-link" href="#"  data-toggle="dropdown">Rahul Singh
+          <a class="nav-link" href="#"  data-toggle="dropdown">Rahul Singh Algar
             <span>/</span>
           </a>
           <!-- Dropdown list -->
