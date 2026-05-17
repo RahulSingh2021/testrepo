c5690f7a6c6efca5a977c521709ccb1839fc3b32
Rahul Home <119575905+Rahulsinghrock1@users.noreply.github.com>
2026-05-17 16:34:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
     <div class="row">
       <div class="col-md-6 align-self-center">
         <div class="copyright-text">
-          <p><a href="#">Eventre</a> &#169; 2017 All Right Reserved5454</p>
+          <p><a href="#">Eventre</a> &#169; 2017 All Right Reserved</p>
         </div>
       </div>
       <div class="col-md-6">
